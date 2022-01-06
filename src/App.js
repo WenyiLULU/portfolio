af91cdcb11043ca2433824c00bc6e1626ec37823
Thibault Santonja <luwenyi1991@gmail.com>
2022-01-06 21:26:30 +0100

    Add main page
    - Update theme
    - Change UI
    - Add main.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import Header from './components/header';
 import Footer from './components/footer';
+import Main from './components/main';
 
 function App() {
   return (
-    <div className="h-screen flex flex-col font-thin bg-gradient-to-br from-primary to-secondary-focus">
+    <div className="h-screen flex flex-col font-thin">
         <Header/>
-        <main className="w-screen grow bg-gradient-to-r from-primary to-secondary px-2 md:px-8 lg:px-[10%] py-2 md:py-4 lg:py-8">
-            <div>
-                <h2 id="me">Hi, I'm Wenyi Lu !</h2>
+        <main className="w-screen grow px-2 md:px-8 lg:px-[10%] py-2 md:py-4 lg:py-8">
+            <div className="bg-neutral/[.9] h-full rounded-box p-4">
+                <Main />
             </div>
         </main>
         <Footer/>
---
+++ src/components/main.js
@@ -0,0 +1,9 @@
+export default function Main() {
+    return (
+        <section className="grid grid-cols-1 gap-36">
+            <h1 className="text-center text-primary uppercase text-5xl font-extralight col-span-full">
+                Hi, I'm Wenyi Lu !
+            </h1>
+        </section>
+    )
+}
