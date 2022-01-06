befb14299a4727aad89b8ec86b85bc46d1643aea
Thibault Santonja <luwenyi1991@gmail.com>
2022-01-06 20:41:29 +0100

    Quick fix HTML balise

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
     <div className="h-screen flex flex-col font-thin bg-gradient-to-br from-primary to-secondary-focus">
         <Header/>
         <main className="w-screen grow bg-gradient-to-r from-primary to-secondary px-2 md:px-8 lg:px-[10%] py-2 md:py-4 lg:py-8">
-
+            <div>
+                <h2 id="me">Hi, I'm Wenyi Lu !</h2>
+            </div>
         </main>
         <Footer/>
     </div>
