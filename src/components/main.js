744df1902e17466566714a40389b5ea5b68f3aca
Thibault Santonja <luwenyi1991@gmail.com>
2022-01-12 23:04:10 +0100

    Pache image tag

--- src/components/main.js
+++ src/components/main.js
@@ -1,24 +1,21 @@
 import me from "../images/me.jpg";
 
 export default function Main(props) {
-
     return (
         <section id="me" className="flex me-wallpaper min-h-screen md:p-20 bg-fixed">
             <div className="self-center hero bg-secondary-focus/75 rounded-tl-3xl rounded-br-3xl">
                 <div className="flex-col hero-content lg:flex-row-reverse">
-                    <img src={me} className="max-w-sm rounded-lg shadow-2xl" alt="my photo" />
                     <div>
                         <h1 className="mb-5 text-5xl font-bold">
                             Hello there
                         </h1>
 
-                        <h1  className="text-center text-primary-content uppercase text-5xl font-extralight col-span-full">
+                        <h1 className="text-center text-primary-content uppercase text-5xl font-extralight col-span-full">
                             Hi, I'm {props.name} Lu !
                         </h1>
                         <p className="mb-5">
                             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
                             exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
-
                         </p>
                     </div>
                 </div>
