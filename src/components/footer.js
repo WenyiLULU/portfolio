8ee77ba3da3bbd6c291b0db2fd15a189acff8713
WenyiLULU <luwenyi1991@gmail.com>
2022-12-09 15:04:35 +0100

    ✏️ fix footer Fixes #9
    - Add email link
    - Change front color to make it more visible
    - Change the section of icons to flex-wrap to make them more responsive

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,25 +1,26 @@
 import icon_github from "../images/icon_Github.png"
-import icon_linkedin from "../images/icon_linkedin.png";
+import icon_linkedin from "../images/icon_linkedin.png"
 import icon_instagram from "../images/icon_Instagram.png"
+import icon_mail from "../images/icon_mail.png"
 
 export default function Footer() {
     return (
-        <footer className="p-4 footer bg-neutral text-base-content footer-center">
+        <footer className="p-4 font-light footer bg-neutral text-secondary footer-center">
             <div className="grid grid-cols-3 justify-items-center">
                 <div >
-                    <p className="text-primary">Host by Vercel</p>
-                    <p className="text-primary">Made by React with Tailwind and DaisyUI</p>
+                    <p className="text-secondary">Hosted by Vercel</p>
+                    <p className="text-secondary">Made by React with Tailwind and DaisyUI</p>
                 </div>
                 <div >
-                    <p className="text-primary">Copyright © 2022 - All right reserved by Wenyi Lu</p>
+                    <p className="text-secondary">Copyright © 2022 - All right reserved by Wenyi Lu</p>
                 </div>
-                <div className="grid grid-cols-3 gap-4">
+                <div className="flex flex-wrap justify-evenly">
                     <a href="https://github.com/WenyiLULU" target="_blank" rel="noopener noreferrer">
                         <button className="btn btn-circle btn-ghost btn-md">
                             <img  src={icon_github} alt="Github" />
                         </button>
                     </a>
-                    <a href="https://www.linkedin.com/in/wenyi-lu-319669138/" target="_blank" rel="noopener noreferrer">
+                    <a href="https://www.linkedin.com/in/wenyi-lu-profile/" target="_blank" rel="noopener noreferrer">
                         <button className="btn btn-circle btn-ghost btn-md">
                             <img  src={icon_linkedin} alt="Linkedin" />
                         </button>
@@ -28,6 +29,13 @@ export default function Footer() {
                         <button className="btn btn-circle btn-ghost btn-md">
                             <img  src={icon_instagram} alt="Instagram" />
                         </button>
+                    </a>
+                    <a href="mailto:luwenyi1991@gmail.com" target="_blank" rel="noopener noreferrer">
+                        <div className="tooltip" data-tip="Send an email to: luwenyi1991@gmail.com">
+                            <button className="btn btn-circle btn-ghost btn-md">
+                                <img  src={icon_mail} alt="Instagram" />
+                            </button>
+                        </div>
                     </a>
 
 
